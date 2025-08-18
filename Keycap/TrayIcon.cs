3b989b935e62846bd3fd8f8f5e851084dcafbdd6
Mykyta <sikachyna.n@gmail.com>
2025-08-18 12:04:50 +0300

    feat: create tray icon

---
+++ Keycap/TrayIcon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.Input;
+using Wpf.Ui.Violeta.Resources;
+using Wpf.Ui.Violeta.Win32;
+
+namespace Keycap
+{
+    internal partial class TrayIcon : IDisposable
+    {
+        private static TrayIcon? _instance;
+        private readonly TrayIconHost _icon;
+        
+        private TrayIcon()
+        {
+            _icon = new()
+            {
+                Icon = GetTrayIcon(),
+                Menu =
+                [
+                    new TrayMenuItem()
+                    {
+                        Header = "Quit",
+                        Command = new RelayCommand(Application.Current.Shutdown)
+                    }
+                ]
+            };
+        }
+
+        private static nint GetTrayIcon()
+        {
+            return Resources.tray_icon.Handle;
+        }
+
+        public static TrayIcon GetInstance()
+        {
+            return _instance ??= new TrayIcon();
+        }
+
+        public void Dispose()
+        {
+            _icon.IsVisible = false;
+        }
+    }
+}
